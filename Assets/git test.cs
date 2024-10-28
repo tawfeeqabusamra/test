bf9b17fc770e506e2fbab05224c0b102e4bd0b4c
tawfeeqabusamra <tefo.w.a.s13@gmail.com>
2024-10-28 09:16:10 +0200

    important

--- Assets/git test.cs
+++ Assets/git test.cs
@@ -7,12 +7,18 @@ public class gittest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int A = 0;
+        while(A<20){
+            int ran = Random.Range(1,21);
+            switch(ran){
+                case 5 : continue;
+                case 15 : break;
+                default : Debug.Log(ran);
+                break;
+                
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
